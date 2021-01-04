e3065417b648fefdfe446d4bd89d0efe8927eda2
AbdalQader mhemed <abdalqader.mhemed@gmail.com>
2021-01-04 15:50:04 +0200

    comment console logs, and use alert insted

--- js/app.js
+++ js/app.js
@@ -13,10 +13,11 @@ while(!isValidInput(colorAnswer) || colorAnswer === ""){
     
 }
 if(colorAnswer === 'yes' || colorAnswer === 'y'){
-    console.log("This is correct, my favorite color is black");
+    //console.log("This is correct, my favorite color is black");
+    alert("This is correct, my favorite color is black");
 }else{
-    console.log("Answer is:" + colorAnswer);
-    console.log("Wrong, my favorite color is balck");
+    //console.log("Wrong, my favorite color is balck");
+    alert("Wrong, my favorite color is balck");
 }
 
 //Q2
@@ -25,9 +26,11 @@ while(!isValidInput(movieAnswer) || movieAnswer === ""){
     movieAnswer = prompt("Do you think that my favorite movie is The Prestige (yes/no), (Y/n?").toLowerCase();
 }
 if(movieAnswer === 'yes' || movieAnswer === 'y'){
-    console.log("This is correct, my favorite movie is The Prestige");
+    //console.log("This is correct, my favorite movie is The Prestige");
+    alert("This is correct, my favorite movie is The Prestige");
 }else{
-    console.log("Wrong, my favorite movie is The Prestige");
+    //console.log("Wrong, my favorite movie is The Prestige");
+    alert("Wrong, my favorite movie is The Prestige");
 }
 
 //Q3
@@ -36,9 +39,11 @@ while(!isValidInput(androidDevelopingAnswer) || androidDevelopingAnswer ===""){
     androidDevelopingAnswer = prompt("Do you think that I'm familiar with Android Development(yes/no), (Y/n?").toLowerCase();
 }
 if(androidDevelopingAnswer === 'yes' || androidDevelopingAnswer === 'y'){
-    console.log("This is correct, I can develop apps in android");
+    //console.log("This is correct, I can develop apps in android");
+    alert("This is correct, I can develop apps in android");
 }else{
-    console.log("Wrong, actually I have good experience in android development");
+    //console.log("Wrong, actually I have good experience in android development");
+    alert("Wrong, actually I have good experience in android development");
 }
 
 //Q4
@@ -47,9 +52,11 @@ while(!isValidInput(germanSpeakAnswer) || germanSpeakAnswer ===""){
     germanSpeakAnswer = prompt("Do you think that I can speak German(yes/no), (Y/n?").toLowerCase();
 }
 if (germanSpeakAnswer === 'yes' || germanSpeakAnswer === 'y'){
-    console.log("Unfortunately I can NOT speak German");
+    //console.log("Unfortunately I can NOT speak German");
+    alert("Unfortunately I can NOT speak German");
 }else{
-    console.log("This is correct I can not speak German");
+    //console.log("This is correct I can not speak German");
+    alert("This is correct I can not speak German");
 }
 
 //Q5
@@ -59,9 +66,11 @@ while(!isValidInput(leaderAnswer) || leaderAnswer ===""){
     leaderQuestion = prompt("Do you think that I am a leader(yes/no), (Y/n?").toLowerCase();
 }
 if(leaderAnswer === 'no' || leaderAnswer === 'n'){
-    console.log("Not quiet, I am a good leader, read the page to know about me");
+    //console.log("Not quiet, I am a good leader, read the page to know about me");
+    alert("Not quiet, I am a good leader, read the page to know about me");
 }else{
-    console.log("This is right, I am good leader");
+    //console.log("This is right, I am good leader");
+    alert("This is right, I am good leader");
 }
 //This function check user input
 function isValidInput(message){
